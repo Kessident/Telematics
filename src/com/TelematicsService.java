8ef6e20031c8300c8987ad9c34e2e75646bbfca2
windspinks <windspinks@gmail.com>
2017-07-21 09:32:21 -0400

    json writing

--- src/com/TelematicsService.java
+++ src/com/TelematicsService.java
@@ -1,6 +1,40 @@
 package com;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TelematicsService {
 
-    static void report(VehicleInfo vehicleInfo){}
+    static void report(VehicleInfo vehicleInfo) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String pathname = vehicleInfo.getVehicleIdentificationNumber() + ".json";
+            mapper.writeValue(new File(pathname), vehicleInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] getFileContents (String fileName) {
+        File file = new File (fileName);
+        try {
+            Scanner fileScanner = new Scanner(file);
+            List<String> fileContents = new ArrayList<>();
+            while (fileScanner.hasNext()) {
+                fileContents.add(fileScanner.nextLine());
+            }
+            return fileContents.toArray(new String[0]); //Converts the list to an array
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not find file *" + fileName + "*");
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
 }
